770734752c964d8d7d42207c8326c837d6c60faa
JSTec1 <johnsouzat@outlook.com>
2025-10-22 13:32:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 export FLASK_APP=backend_upload.py
 export FLASK_ENV=production
-gunicorn backend_upload:app --bind 0.0.0.0:5000
+gunicorn backend_upload:app --bind 0.0.0.0:$PORT
+
