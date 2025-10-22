c83b94d9256d77e71846181eba0dd9808bad9a5f
JSTec1 <johnsouzat@outlook.com>
2025-10-22 18:02:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const streamifier = require("streamifier");
 const cors = require("cors");
 
 // Configuração do Cloudinary
-cloudinary.config({
+console.log("Cloudinary config:", {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
