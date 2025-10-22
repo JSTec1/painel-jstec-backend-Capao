a392a53b1dc7f913d472954445a19bd4a46390c1
JSTec1 <johnsouzat@outlook.com>
2025-10-22 17:54:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const cors = require("cors");
 
 // Configuração do Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const app = express();
